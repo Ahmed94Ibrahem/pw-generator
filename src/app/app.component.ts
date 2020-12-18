a714cdc4a91c68320640f3548a8deda2fe6aa64d
Ahmed Ibrahem <ahmedibrahema061@gmail.com>
2020-12-18 09:47:17 +0200

    Finishing Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-generator';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  inputLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+
+  }
+
+  useLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+  
+  useNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  
+  useSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onButtonClick(){
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+
+  }
 }
